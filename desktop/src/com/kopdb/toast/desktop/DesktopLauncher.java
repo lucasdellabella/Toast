d161699c1c554d4f993805782ded38f0886d6c27
Lucas Della Bella <dellabella.lucas@gmail.com>
2016-12-30 15:58:32 -0500

    Initialize empty libgdx project

---
+++ desktop/src/com/kopdb/toast/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kopdb.toast.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kopdb.toast.ToastGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ToastGame(), config);
+	}
+}
